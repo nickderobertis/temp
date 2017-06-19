49ea325fe0c84fe2305937fe73eda0cc3b310add
System <none>
2017-06-19 00:06:32 +0000

    👩⏰ Updated with Glitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: `app/app.component.html`,
+  styleUrls: ['app/app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  wrapSlides: boolean = true;
+	myInterval: number = 1000;
+	images:Array<Object> = [];
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.images.push({imageIdentifier: 'sample'});
+      this.images.push({imageIdentifier: 'couple'});
+      this.images.push({imageIdentifier: 'nice_couple'});      
+    }, 5000)
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// Cloudinary module
+import { CloudinaryModule, CloudinaryConfiguration, provideCloudinary } from '@cloudinary/angular';
+import { Cloudinary } from 'cloudinary-core';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+
+
+const cloudinaryLib = {
+  Cloudinary: Cloudinary
+};
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+		CloudinaryModule.forRoot(cloudinaryLib, {
+      cloud_name: 'demo'
+    } as CloudinaryConfiguration),
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
