d428b46eb64819df051bf8376e74e239c7ae92f5
Nick DeRobertis <Nick DeRobertis>
2017-06-18 20:26:30 -0400

    Removed cloudinary packages and upgraded typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: `app/app.component.html`,
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent implements OnInit {
-  wrapSlides: boolean = true;
-	myInterval: number = 1000;
-	images:Array<Object> = [];
-  
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.images.push({imageIdentifier: 'sample'});
-      this.images.push({imageIdentifier: 'couple'});
-      this.images.push({imageIdentifier: 'nice_couple'});      
-    }, 5000)
-  }  
+export class AppComponent {
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// Cloudinary module
-import { CloudinaryModule, CloudinaryConfiguration, provideCloudinary } from '@cloudinary/angular';
-import { Cloudinary } from 'cloudinary-core';
 
 import { AppComponent }  from './app.component';
 
-
-const cloudinaryLib = {
-  Cloudinary: Cloudinary
-};
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     NgbModule.forRoot(),
-		CloudinaryModule.forRoot(cloudinaryLib, {
-      cloud_name: 'demo'
-    } as CloudinaryConfiguration),
   ],
   declarations: [
     AppComponent
